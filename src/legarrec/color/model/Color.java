0daf4956ea7eab44604d493afc287004e3934b0c
BeeLeg <bertrand.legarrec.fr@gmail.com>
2022-03-01 11:57:55 +0100

    Ajout de tests constructors et exceptions

--- src/legarrec/color/model/Color.java
+++ src/legarrec/color/model/Color.java
@@ -1,2 +1,46 @@
-package legarrec.color.model;public class Color {
+package legarrec.color.model;
+
+import java.util.regex.Pattern;
+
+public class Color {
+
+    public int rouge;
+    public int vert;
+    public int bleu;
+    public String hexa;
+
+    public Color(int rouge, int vert, int bleu) throws IllegalArgumentException {
+        if((rouge <= 255 && vert <= 255 && bleu <= 255) && (rouge >= 0 && vert >= 0 && bleu >= 0)){
+            this.rouge = rouge;
+            this.vert = vert;
+            this.bleu = bleu;
+        } else
+            throw new IllegalArgumentException("Valeur doit être comprise entre 0 et 255");
+    }
+
+    public int getRouge(){
+        return rouge;
+    }
+    public int getVert(){
+        return vert;
+    }
+    public int getBleu(){
+        return bleu;
+    }
+
+
+    public Color(String hexa) {
+        if(isHexaFormat(hexa))
+            this.hexa = hexa;
+        else
+            throw new IllegalArgumentException("Mauvais format de couleur Hexa");
+    }
+
+    public String getHexa(){
+        return hexa;
+    }
+
+    public boolean isHexaFormat(String string) {
+        return Pattern.matches("^#[0-9A-F]{1,6}+$", string);
+    }
 }
--- test/legarrec/color/model/ColorTest.java
+++ test/legarrec/color/model/ColorTest.java
@@ -1,4 +1,50 @@
+package legarrec.color.model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ColorTest {
-  
+    private Color color1;
+    private Color color2;
+
+    @BeforeEach
+    public void setUp(){
+        color1 = new Color(10,0,255);
+        color2 = new Color("#123456");
+    }
+
+    @Test
+    public void testColorConstructor1() {
+        assertEquals(10, color1.getRouge());
+        assertEquals(0, color1.getVert());
+        assertEquals(255, color1.getBleu());
+    }
+
+    @Test
+    public void testColorConstructor2(){
+        assertEquals("#123456", color2.getHexa());
+    }
+
+    @Test
+    public void testInvalideArgumentConstructor1(){
+        assertThrows(IllegalArgumentException.class, () -> new Color(-1, 30, 25));
+    }
+
+    @Test
+    public void testRegEx(){
+        assertTrue(color2.isHexaFormat("#123456"));
+    }
+
+    @Test
+    public void testInvalideArgumentConstructor2(){
+        assertThrows(IllegalArgumentException.class, () -> new Color("123456"));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        color1 = null;
+    }
 }
