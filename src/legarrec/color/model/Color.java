8167c743810853c70d052a30e56706a71a442185
BeeLeg <bertrand.legarrec.fr@gmail.com>
2022-03-01 12:29:12 +0100

    ajout tests exceptions et tostring

--- src/legarrec/color/model/Color.java
+++ src/legarrec/color/model/Color.java
@@ -28,6 +28,27 @@ public class Color {
         return bleu;
     }
 
+    public void setRouge(int rouge){
+        if(rouge <= 255  && rouge >= 0 ){
+            this.rouge = rouge;
+        } else
+            throw new IllegalArgumentException("Valeur doit être comprise entre 0 et 255");
+    }
+
+    public void setVert(int vert){
+        if(vert <= 255  && vert >= 0 ){
+            this.vert = vert;
+        } else
+            throw new IllegalArgumentException("Valeur doit être comprise entre 0 et 255");
+    }
+
+    public void setBleu(int bleu){
+        if(bleu <= 255  && bleu >= 0 ){
+            this.bleu = bleu;
+        } else
+            throw new IllegalArgumentException("Valeur doit être comprise entre 0 et 255");
+    }
+
 
     public Color(String hexa) {
         if(isHexaFormat(hexa))
@@ -40,7 +61,16 @@ public class Color {
         return hexa;
     }
 
+    public void setHexa(String hexa){
+        this.hexa = hexa;
+    }
+
     public boolean isHexaFormat(String string) {
         return Pattern.matches("^#[0-9A-F]{1,6}+$", string);
+    }
+
+    @Override
+    public String toString() {
+        return "[value="+getHexa()+", r="+getRouge()+", g="+getVert()+", b="+getBleu()+"]";
     }
 }
--- test/legarrec/color/model/ColorTest.java
+++ test/legarrec/color/model/ColorTest.java
@@ -24,13 +24,34 @@ class ColorTest {
     }
 
     @Test
+    public void testSetColors(){
+        color1.setRouge(30);
+        color1.setVert(30);
+        color1.setBleu(30);
+        assertEquals(30,color1.getRouge(), "La couleur rouge est pas bonne");
+        assertEquals(30,color1.getVert(), "la couleur verte est pas bonne");
+        assertEquals(30,color1.getBleu(), "la couleur bleue est pas bonne");
+    }
+
+    @Test
     public void testColorConstructor2(){
         assertEquals("#123456", color2.getHexa());
     }
 
     @Test
+    public void testSetHexa(){
+        color2.setHexa("#654321");
+        assertEquals("#654321", color2.getHexa(), "l'Hexa n'est pas bon");
+    }
+
+    @Test
     public void testInvalideArgumentConstructor1(){
         assertThrows(IllegalArgumentException.class, () -> new Color(-1, 30, 25));
+        IllegalArgumentException excep = assertThrows(IllegalArgumentException.class, () -> color1.setRouge(900));
+        assertEquals("Valeur doit être comprise entre 0 et 255", excep.getMessage());
+
+        assertThrows(IllegalArgumentException.class, () -> color1.setVert(-20));
+        assertThrows(IllegalArgumentException.class, () -> color1.setBleu(900));
     }
 
     @Test
@@ -43,8 +64,15 @@ class ColorTest {
         assertThrows(IllegalArgumentException.class, () -> new Color("123456"));
     }
 
+    @Test
+    public void testToString(){
+        color1.setHexa(color2.getHexa());
+        assertEquals("[value=#123456, r=10, g=0, b=255]", color1.toString());
+    }
+
     @AfterEach
     public void tearDown() {
         color1 = null;
+        color2 = null;
     }
 }
